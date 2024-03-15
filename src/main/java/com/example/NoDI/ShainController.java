bae35bcb727b3028cdc96c03e2ed96dd318ffb0b
kitou88C39 <kanfutrooper@icloud.com>
2024-03-15 15:33:31 +0900

    ShainControllerの実装

---
+++ src/main/java/com/example/NoDI/ShainController.java
@@ -0,0 +1,34 @@
+package com.example.NoDI;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ShainController {
+
+    @RequestMapping("/input")
+    public String index() {
+        return "index.html";
+    }
+
+    @RequestMapping("/output")
+    public String result(@Validated ShainForm shainForm,
+            BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "index.html";
+        }
+
+        // String name = "コントローラー太郎";
+        // サービス層から社員を検索
+        ShainService shainService = new ShainServiceImpl();
+        String name = shainService.findByNo(shainForm.getNumber());
+
+        model.addAttribute("number", shainForm.getNumber());
+        model.addAttribute("name", name);
+        return "output.html";
+    }
+
+}
