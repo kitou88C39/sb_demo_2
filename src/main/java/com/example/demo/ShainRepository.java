e5ebcd70fa806dc4597147c38f4f72122f09180f
kitou88C39 <kanfutrooper@icloud.com>
2024-03-15 15:47:47 +0900

    ShainRepositoryの実装

--- src/main/java/com/example/demo/ShainRepository.java
+++ src/main/java/com/example/demo/ShainRepository.java
@@ -1,7 +1,23 @@
 package com.example.demo;
 
-public interface ShainRepository {
+public class ShainRepositoryImpl implements ShainRepository {
 
-    String selectByNo(String number);
+    @Override
+    // 今回はDBに接続していないが、DBに接続したつもりで
+    public String selectByNo(String number) {
+        String name;
+        switch (number) {
+            case "100":
+                name = "佐藤";
+                break;
+            case "101":
+                name = "鈴木";
+                break;
+            default:
+                name = "登録されていません";
+                break;
+        }
+        return name;
+    }
 
 }
