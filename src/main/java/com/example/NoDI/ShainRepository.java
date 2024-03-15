a3a528a0659444c945e57256b44d20eec6ef90f3
kitou88C39 <kanfutrooper@icloud.com>
2024-03-15 16:20:23 +0900

    ShainRepositoryの実装

--- src/main/java/com/example/NoDI/ShainRepository.java
+++ src/main/java/com/example/NoDI/ShainRepository.java
@@ -2,4 +2,22 @@ package com.example.NoDI;
 
 public class ShainRepository {
 
+    @Override
+    // 今回はDBに接続していないが、DBに接続したつもりで
+    public String selectByNo(String number) {
+        String name;
+        switch (number) {
+            case "100":
+                name = "佐藤";
+                break;
+            case "101":
+                name = "鈴木";
+                break;
+            default:
+                name = "登録されていません";
+                break;
+        }
+        return name;
+    }
+
 }
